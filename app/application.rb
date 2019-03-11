f75ed18212973d73c4f3e58cb2c3ff3b86526ee6
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-11 15:19:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,3 +3,14 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    
+    if req.path=="/items/<ITEM NAME>"
+      resp.write item.price 
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+ 
+    resp.finish
+  end
+end
